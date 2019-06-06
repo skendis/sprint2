e84781a93bbd3cff3623f6b2327b8d1145e1bd7a
skendis <sergei.kendis@gmail.com>
2019-06-06 18:31:14 +0300

    gallery-fix

--- js/canvas-controller.js
+++ js/canvas-controller.js
@@ -2,30 +2,19 @@
 let canvas;
 let ctx;
 
-var gMeme = {
-    selectedImgId: 5,
-    txts: [
-        {
-            line: 'I never eat falafel',
-            size: 3,
-            align: 'center',
-            color: 'white',
-            font: 'impact'
-        },
-        {
-            line: 'I never eat falafel again',
-            size: 3,
-            align: 'center',
-            color: 'white',
-            font: 'impact'
-        }
-    ]
-}
 function init() {
     renderCanvas();
 }
 
+function changeImg(id){
+    gMeme.selectedImgId = id;
+    renderCanvas();
 
+}
+function onChangeText(line,text){
+    changeText(text,line);
+    renderCanvas();
+}
 
 function renderCanvas() {
     canvas = document.querySelector('#canvas');
@@ -33,16 +22,16 @@ function renderCanvas() {
     var myImg = new Image();
 
     myImg.onload = function () {
-        canvas.width = myImg.width;
-        canvas.height = myImg.height;
-        ctx.drawImage(myImg, 0, 0, myImg.width, myImg.height)
+        canvas.width =300;
+        canvas.height = 300;
+        ctx.drawImage(myImg, 0, 0, canvas.width, canvas.height)
         ctx.shadowOffsetX = 2;
         ctx.shadowOffsetY = 2;
         ctx.shadowBlur = 1;
         ctx.shadowColor = 'black';
 
         for (var i = 0; i < gMeme.txts.length; i++) {
-            ctx.font = `${gMeme.txts[i].size}rem ${gMeme.txts[i].font}`;
+            ctx.font = `${gMeme.txts[i].size}em ${gMeme.txts[i].font}`;
             ctx.fillStyle = gMeme.txts[i].color;
             ctx.textAlign = gMeme.txts[i].align;
             ctx.fillText(gMeme.txts[i].line, canvas.width / 2, i === 0 ? 70 : (canvas.height - 30));
