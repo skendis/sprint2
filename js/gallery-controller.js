3a3e0d8d58b6e4f649ea70d6c5da88ea30e90e73
Tamir Levital <tamir.levital@gmail.com>
2019-06-06 16:44:15 +0300

    misc. amends

--- js/gallery-controller.js
+++ js/gallery-controller.js
@@ -2,6 +2,17 @@
 
 //TODO:RENDER IMAGES TO CONTAINER
 
+function renderImages() {
+    var images = getImagesForDisplay()
+    var strHtmls = images.map(function(image) {
+        return `
+        <img src="images/${image.id}.jpg onclick ="openEditor(${image.id})">
+        `
+    })
+    $('.gallery').html(strHtmls.join(''))
+}
+
+
 //TODO:OPEN EDITOR WITH CLICKED IMG
 
 //TODO:RENDER IMAGES BY FILTER
--- js/main.js
+++ js/main.js
@@ -1,19 +1,49 @@
 'use strict'
 
 let gFontSize = 16
-let textPos = 0
+let gTextPos = 0
+let gCtx
+
+
+function addText(ev) {
+    console.log(ev)
+    gCtx.save()
+        // const offsetX = ev.offsetX
+        // const offsetY = ev.offsetY
+    const { offsetX, offsetY } = ev
+
+    drawText('test', offsetX, offsetY)
+
+}
+ctx.restore()
+}
 
 function getColor() {
     let fontColor = document.querySelector('#font-color').value
     return fontColor
 }
 
-function increaseFontSizeBy1px(ev) {
+function increaseFontSize(ev) {
     ev = gFontSize++
         console.log(gFontSize, 'gFontSize');
 }
 
-function decreaseFontSizeBy1px(ev) {
+function decreaseFontSize(ev) {
     ev = gFontSize--
         console.log(gFontSize, 'gFontSize');
 }
+
+function elevateText(ev) {
+    // if (gTextPos >= ) // needs to add max height
+    ev = gTextPos++
+        console.log(gTextPos, 'gTextPos');
+}
+
+function lowerText(ev) {
+    ev = gTextPos--
+        console.log(gTextPos, 'gTextPos');
+}
+
+// function clearLine(){
+
+// }
